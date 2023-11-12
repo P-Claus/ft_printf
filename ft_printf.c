343df05f459c21f332386ba6c7559e3ad4c86d7d
Pieter Claus <pieter.claus1@gmail.com>
2023-11-12 10:07:27 +0100

    create main.c and ft_printf.c

---
+++ ft_printf.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 09:49:58 by pclaus            #+#    #+#             */
+/*   Updated: 2023/11/12 10:06:52 by pclaus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		count;
+
+	va_start(ap, format);
+	count = 0;
+	while (*format)
+	{
+		if (*format == %)
+			count += print_format(*(++format), ap);
+		else
+			count += write(1, format, 1);
+		++format;
+	}
+	va_end(ap);
+	return (count);
+}
