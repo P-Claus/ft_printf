f2554ad0a8def93b116d0cbdb918e59d03116f62
Pieter Claus <pieter.claus1@gmail.com>
2023-11-13 21:56:58 +0100

    print char works but doesn't print count

--- main.c
+++ main.c
@@ -6,16 +6,21 @@
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 09:43:55 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/12 10:41:34 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/13 21:50:08 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <stdio.h>
 
 int	main(void)
 {
 	int	count;
-
-	count = ft_printf("The char is:	%c\n", "H");
-	ft_printf("The chars written are: %d\n", count);
+	//int	count2;
+	
+	count = ft_printf("The char is: %c\n", 'm');
+	printf("The amount of chars is: %d\n", count);
+	//ft_printf("The chars written are: %d\n", count);
+	//printf("The char is: %c\n", 'h');
+	//printf("The char written is: %d\n", count2);
 }
--- print_format.c
+++ print_format.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 10:07:47 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/12 10:36:41 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/13 21:40:21 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	print_format(char specifier, va_list ap)
 	/*else if (specifier == 's')
 		count += print_string(va_arg(ap, char *));
 	else if (specifier == 'd')
-		count += print_digit(va_arg(ap, int), 10);
+		count += print_digit(long(va_arg(ap, int)), 10);
 	else if (specifier == 'x')
-		count += print_digit(va_arg(ap, unsigned int), 16);
+		count += print_digit(long(va_arg(ap, unsigned int)), 16);
 	else
 		count += write(1, &specifier, 1); //writes specifier is it's a weird one (temporary)*/
 	return (count);
