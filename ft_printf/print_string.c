116375e9d366bd921c4535b3c11dbce466688148
Pieter Claus <pieter.claus1@gmail.com>
2023-11-18 16:25:58 +0100

    change when str=0

--- ft_printf/print_string.c
+++ ft_printf/print_string.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 18:57:16 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/18 15:17:07 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/18 16:25:27 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	print_string(char *str)
 	int	count;
 
 	count = 0;
+	if (!str)
+		return (0);
 	while (*str)
 	{
 		print_character((int)*str);
