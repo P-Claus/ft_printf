cc2c2bbfb69e1eb29cb89363a9cc2c422d3c768c
Pieter Claus <pieter.claus1@gmail.com>
2023-11-18 16:57:38 +0100

    update print_str function to work with empty string or null string

--- ft_printf/print_string.c
+++ ft_printf/print_string.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 18:57:16 by pclaus            #+#    #+#             */
-/*   Updated: 2023/11/18 16:56:00 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/11/18 16:57:27 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ int	print_string(char *str)
 
 	count = 0;
 	if (!str)
-		return ("(null)");
+	{
+		print_string("(null)");
+		return (6);
+	}
 	while (*str)
 	{
 		print_character((int)*str);
